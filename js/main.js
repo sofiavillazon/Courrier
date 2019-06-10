4c58e309e89c319cc238e754b4c7d7a9e394cf76
sofia <sofiavillazon@gmail.com>
2019-06-10 17:25:39 -0500

    chat

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// HANDLER
+$('.handler').on('click', function(){
+    $('.detailOrder').toggleClass('oculto').siblings().removeClass('oculto');
+    $('.stateOrder').toggleClass('oculto').siblings().removeClass('oculto');
+
+});
+// HANDLER
+
+$('.handler2').on('click', function(){
+    $('#chat').toggleClass('oculto');
+});
+
+// HANDLER
+$('.handlerSide').on('click', function(){
+    $('.sidebar').toggleClass('oculto').siblings().removeClass('oculto');
+});
+
+// HANDLER MOBILE
+$('.handlerSide2').on('click', function(){
+    $('.sidebar').toggleClass('ocultomov').siblings().removeClass('ocultomov');
+});
+
+$('.pinmap').on('click', function(){
+    $(this).parent().toggleClass('active').siblings().removeClass('active');
+});
+
+function despejarMapa() {
+    if( $('.detailOrder').hasClass('oculto') ){
+        $('.detailOrder').removeClass('oculto');
+        $('.stateOrder').removeClass('oculto');
+    }else{
+        event.preventDefault();
+        $('.detailOrder').fadeOut( 100 );
+        $('.detailOrder').fadeIn( 100 );
+        $('.stateOrder').fadeOut( 200 );
+        $('.stateOrder').fadeIn( 200 );
+        // $(".detailOrder .cont").effect( "shake", { direction: "up", times: 4, distance: 10}, 1000 );
+    }
+}
+
+
+// DD
+$('.sidebar .order').on('click', function(){
+    despejarMapa();
+    $(this).toggleClass('active').siblings().removeClass('active');
+    $('.opOne').addClass('active');
+    $('.opTwo').removeClass('active');
+    event.preventDefault();
+});
+
+$('.detailOrder .order').on('click', function(){
+    if( $(this).hasClass('active') ){
+        event.preventDefault();
+    }else{
+        $(this).toggleClass('active').siblings().removeClass('active');
+    }
+});
+
+// CLOSE ORDER
+$('.asignar').on('click', function(){
+    despejarMapa();
+    $('.opTwo').addClass('active');
+    $('.opOne').removeClass('active');
+   
+    if (!e) var e = window.event;
+    e.cancelBubble = true;
+    if (e.stopPropagation) e.stopPropagation();
+});
+
+function newMessage() {
+    message = $(".cajaTexto input").val();
+    if($.trim(message) == '') {
+      return false;
+    }
+    $('<li class="enviado"><p>' + message + '</p></li>').appendTo($('.mensajes ul'));
+    $('.cajaTexto input').val(null);
+    $('.cardOrder.active .detalle').html( message);
+    $(".scrolledarea").animate({ scrollTop: $(document).height() }, "fast");
+  };
+  
+  $('.submit22').click(function() {
+    newMessage();
+  });
+  
+  $(window).on('keydown', function(e) {
+    if (e.which == 13) {
+      newMessage();
+      return false;
+    }
+});
+
+
+$('.btn-ped').on('click', function(){
+    $("body").animate({ scrollTop: 1200 }, "fast");
+    
+});
